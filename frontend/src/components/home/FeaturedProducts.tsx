905f89ed21e33cead991c18311c10b7362b9574f
peluca6666 <gercab666@gmail.com>
2025-10-17 00:13:49 -0300

    mejora fondo

--- frontend/src/components/home/FeaturedProducts.tsx
+++ frontend/src/components/home/FeaturedProducts.tsx
@@ -39,7 +39,7 @@ export default function FeaturedProducts() {
   }
 
   return (
-    <section className="bg-gray-50 py-12 sm:py-20">
+    <section className="bg-white py-12 sm:py-20">
       
       <div className="container mx-auto px-4 sm:px-12">
         <div className="text-center mb-12 sm:mb-16">
