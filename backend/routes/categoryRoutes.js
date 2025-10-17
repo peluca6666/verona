2ec160bd87fc656e9b4956d1ee437df53094a19c
peluca6666 <gercab666@gmail.com>
2025-10-17 00:01:14 -0300

    Add GET /admin route for categories

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -5,6 +5,7 @@ import authMiddleware from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get('/' , CategoryController.getCategories);
+router.get('/admin', authMiddleware, CategoryController.getCategories);
 router.post('/admin', authMiddleware, CategoryController.createCategory);
 router.put('/admin/:id', authMiddleware, CategoryController.updateCategory);
 router.delete('/admin/:id', authMiddleware, CategoryController.deleteCategory);
